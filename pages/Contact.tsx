ab0bcbdfd5b50504c10bb180048a51d13c417695
PixelByteOfficial <pixelbyteoffical@gmail.com>
2025-09-23 20:09:25 +0530

    Update Contact.tsx

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -63,15 +63,15 @@ const Contact: React.FC = () => {
                                 <svg className="w-6 h-6 text-aqua-teal mr-4 flex-shrink-0 mt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                                 <div>
                                     <h3 className="text-lg font-semibold text-deep-navy">Our Address</h3>
-                                    <p>TamilNadu</p>
-                                    <p>India</p>
+                                    <p>81/268,SakkarammanKadu, Bhavani</p>
+                                    <p>Erode - 638301</p>
                                 </div>
                             </div>
                             <div className="flex items-center">
                                 <svg className="w-6 h-6 text-aqua-teal mr-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
                                 <div>
                                     <h3 className="text-lg font-semibold text-deep-navy">Phone Numbers</h3>
-                                    <p>7010071477</p>
+                                    <p>9488537907</p>
                                     <p>9445698873</p>
                                 </div>
                             </div>
